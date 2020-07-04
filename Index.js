5a3148e6f915e2ee30113c8ec3f626a16136da67
jabeeralsabah <67536396+jabeeralsabah@users.noreply.github.com>
2020-07-04 18:12:38 +0530

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,6 @@
+
+        console.log("Hello world...!!!");
+        document.write("Hello world, this is your world...!!!");
+        console.error("This is error");
+        console.warn("Hi, This is warning...!!!!");
+        //console.assert(2==2); 
