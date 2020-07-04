914288c85852bffa9c8a59aa4124bb320f710e04
jabeeralsabah <67536396+jabeeralsabah@users.noreply.github.com>
2020-07-04 19:06:36 +0530

    Add files via upload

---
+++ JavascriptVariables&Datatypes.js
@@ -0,0 +1,33 @@
+//Javascript Variables 
+var a = 10;
+var b=40;  // numaric datatype
+var str = 'Jabeer';
+var str1="ALSabah"; //string datatype
+ char = 'x'; //charecter datatype
+obj = {
+        name:'Jabeer',
+        profession: 'Engineer'
+} // object datatype
+var arr = ['a',12,"Ramu"]; // array datatype
+var s;
+var d = null;
+var boolvalue = true;
+console.log("This is value of a: ",a);
+console.log("This is value of b: ",b);
+console.log("This is value of str: ", str);
+console.log("This is value of str1: ", str1);
+console.log("This is your object : ", obj);
+console.log("This is your array : ", arr);
+console.log("This is your array 2 nd value: ",arr[2]);
+console.log("This is your s : ",s);
+console.log("This is your d : ", d);
+console.log("This is your boolvalue : ",boolvalue);
+//typeof will specify typeof datavalue
+console.log(typeof a); 
+console.log(typeof str);
+console.log(typeof char);
+console.log(typeof obj);
+console.log(typeof arr);
+console.log(typeof s);
+console.log(typeof d);
+console.log(typeof boolvalue);
