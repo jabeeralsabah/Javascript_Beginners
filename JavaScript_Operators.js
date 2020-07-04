b7e2734c10a1c74f65c03262ceaa3c08b744ecd6
jabeeralsabah <67536396+jabeeralsabah@users.noreply.github.com>
2020-07-04 21:09:45 +0530

    Add files via upload

---
+++ JavaScript_Operators.js
@@ -0,0 +1,25 @@
+
+//Arthematic operator
+var value1 = 10;
+var value2 = 30;
+var value3 = 40;
+//Assignment operator ---- +, - 
+value3 += 3;
+value3 -=4;
+console.log("Addition of value1 and value2 are : ",value1+value2);
+console.log("Subraction of value2 and value1 are ", value2-value1);
+console.log("Division of two numbers : ", value2/value1);
+console.log("Multiplication of two numbers :", value1*value2);
+console.log("Vlaue3 result is ",value3);
+
+//Comaprision operator  >,<,<=,>=,==
+console.log(value1=value2);
+console.log(value1>value3);
+console.log(value3<value2);
+console.log(value1<=value3);
+
+//Logical operator -- && , ||
+console.log(true && false);
+console.log(true || false);
+console.log(false && false);
+
