2ab1a1ad9bbae4d12f63879e273855a949d1ba7b
jabeeralsabah <67536396+jabeeralsabah@users.noreply.github.com>
2020-07-04 22:47:51 +0530

    Add files via upload

---
+++ JavaScript_Functions.js
@@ -0,0 +1,19 @@
+//Function in JavaScipt 
+function Addition(a,b){
+    document.writeln("Addition of 2 numbers : ",a+b);
+}
+function subtraction(a,b){
+    document.writeln("Subtraction of both numbers :", a-b );
+}
+function multiplication(a,b){
+    document.writeln("Multiplication of 2 numbers :", a*b );
+}
+function division(a,b){
+    document.writeln("division of 2 numbers : ", a/b );
+}
+var a =20;
+var b = 10;
+console.log(Addition(a,b)); 
+console.log(subtraction(a,b));
+console.log(division(a,b));
+console.log(multiplication(a,b));
