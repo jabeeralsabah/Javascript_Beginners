40f8110cde40df89158b5823ae6c4a2fffe042b1
jabeeralsabah <67536396+jabeeralsabah@users.noreply.github.com>
2020-07-05 11:00:24 +0530

    Add files via upload

---
+++ JavaScript_StringMethods.js
@@ -0,0 +1,14 @@
+var str = "Shannu is very good girl ";
+ console.log(str.concat(", am i right?")); // we can concat to the string.
+ console.log(str.indexOf('i')); // return the first occurance of value in string.
+ console.log(str.lastIndexOf('i')); //it returns last occurance of value in sstring.
+ console.log(str.replace("Shannu","Shaheen")); //it will replace given string with existing string.
+ console.log(str.split('is')); //Split a string into substrings using the specified separator and return them as an array.
+ console.log(str.toLocaleLowerCase()); // return string in lowercase.
+ console.log(str.toLocaleUpperCase());// returns string in uppercase.
+ console.log(str.toUpperCase()); //returns string in uppercase.
+ console.log(str.trim()); //it will trim left and right spaces.
+ console.log(str.slice(5,8)); //it returns part of substring based on given index range.
+
+
+
